7753e8b29332b8dc298df75044f505cf82db3564
acuccuru <arnaud.cuccuru@cea.fr>
2022-05-31 16:19:42 +0200

    First version
    first version of model and KB Tea

---
+++ test/KB/tea.pl
@@ -0,0 +1,80 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+
+server(Port) :-
+	http_server(http_dispatch, [port(Port)]).
+
+/** wilting - flétrissement 
+wilting(whiteTea) se lit : La production du whiteTea passe par une étape de wilting
+idem pour les autres facts
+*/
+wilting(whiteTea).
+wilting(oolongTea).
+wilting(blackTea).
+
+/** bruising - écrasement */
+bruising(Tea) :-
+	tossingInBasket(Tea) ;
+	lightCrushing(Tea) ;
+	cTCwithFullOxidation(Tea).
+tossingInBasket(oolongTea).
+lightCrushing(blackTea).
+
+/** oxidation - oxydation */
+oxidation(Tea) :-
+	partialOxidation(Tea) ;
+	fullOxidation(Tea) ;
+	cTCwithFullOxidation(Tea);
+	qsdf(Tea).
+partialOxidation(oolongTea).
+fullOxidation(blackTea).
+
+cTCwithFullOxidation(blackTea).
+
+/** fixation - fixation */
+fixation(Tea) :-
+	steaming(Tea) ;
+	panning(Tea) ;
+	baking(Tea).
+steaming(greenTea).
+panning(greenTea).
+panning(yellowTea).
+panning(oolongTea).
+baking(whiteTea).
+baking(oolongTea).
+
+/** yellowing - jaunissement */
+yellowing(Tea) :-
+	sweltering(Tea).
+sweltering(yellowTea).
+
+/** shaping - mise en forme */
+shaping(Tea) :-
+	rolling(Tea) ;
+	forming(Tea) ;
+	lightRolling(Tea) ;
+	ballRolling(Tea).
+rolling(greenTea).
+rolling(yellowTea).
+rolling(oolongTea).
+rolling(blackTea).
+forming(greenTea).
+lightRolling(whiteTea).
+ballRolling(oolongTea).
+
+/** drying - séchage */
+drying(postFermentedTea).
+drying(greenTea).
+drying(yellowTea).
+drying(whiteTea).
+drying(oolongTea).
+drying(blackTea).
+
+/** curing - fumage */
+curing(Tea) :- 
+	naturalAging(Tea) ; 
+	wetPiling(Tea) ; 
+	firing(Tea).
+naturalAging(postFermentedTea).
+wetPiling(postFermentedTea).
+firing(oolongTea).
